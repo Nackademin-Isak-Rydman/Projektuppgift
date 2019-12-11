37d056ea2468eb5437798d9d7a2dfe7df696409b
nackademin-isak-rydman <isak.rydman@yh.nackademin.se>
2019-12-11 11:32:36 +0100

    Uppdaterat servern

--- klient.py
+++ klient.py
@@ -1,1 +1,18 @@
-#klient
+import socket
+
+host = "127.0.0.1"
+port = 12345
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+
+while True:
+    data = sock.recv(1024)
+    print("mottaget meddelande: ", str(data.decode('ascii')))
+
+    ans = input('fortsätt? ja/nej:')
+    if ans == "y":
+        continue
+    else:
+        break
+sock.close()
