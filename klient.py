833c46deb098655f9bddad1beb47e4ed2a2e8f54
nackademin-isak-rydman <isak.rydman@yh.nackademin.se>
2019-12-17 11:35:23 +0100

    Uppdaterat klienten

--- klient.py
+++ klient.py
@@ -1,18 +1,29 @@
 import socket
 
-host = "127.0.0.1"
-port = 12345
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, port))
-
-while True:
-    data = sock.recv(1024)
-    print("mottaget meddelande: ", str(data.decode('ascii')))
-
-    ans = input('fortsätt? ja/nej:')
-    if ans == "y":
-        continue
+def check_message(data):
+    if len(data) > 0:
+        return True
     else:
-        break
-sock.close()
+        return False
+  
+def Main(): 
+    host = "127.0.0.1"
+    port = 12345
+    my_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    my_socket.connect((host,port)) 
+  
+    message = "Välkommen"
+    while True: 
+        message = input("-> ")
+        my_socket.send(message.encode('utf-8')) 
+        data = my_socket.recv(1024)
+        true_or_false = check_message(data)
+        if true_or_false:
+            print("Mottaget från servern:",str(data.decode('utf-8')))
+        else:
+            print("Meddelandet var tomt.")
+
+    my_socket.close() 
+  
+if __name__ == '__main__':
+    Main() 
